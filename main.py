89d9882d6c5a2662e4334d364fd5dbee22e7310a
martin milon <martin.milon@ensc.fr>
2021-06-03 14:39:47 +0200

    clean up

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def random_number_list(list_size):
             k=list_size
         ) # choose 'list_size' element at random from -1000 to 1000 (inclusive)
 
-def run():
-    for number in random_number_list(20): # for each number in a list of 20 randoms element between -1000 and 1000
+def run(list_of_number):
+    for number in list_of_number:
         if number % 15 == 0: # if the number is divisible by 15, print Gestform and move on to the next number
             print('Gestform')
             continue
@@ -22,4 +22,6 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    numbers_to_print = random_number_list(20) # a list of 20 randoms element between -1000 and 1000
+    # you can simply replace it by any given list of integers for more practical tests
+    run(numbers_to_print)
